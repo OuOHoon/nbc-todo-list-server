bbc74616b31c2955eb11e52d268e9bb4ebc06b83
OuOHoon <wls2579@gmail.com>
2023-11-11 21:23:15 +0900

    initial commit

---
+++ src/main/java/com/sparta/todolistserver/TodoListServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todolistserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/todolistserver/TodoListServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.todolistserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
